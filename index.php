b702e57a7de2f7c3c4c5f0bbac4031999ffc097f
ismaelfari1996 <abel_fari@hotmail.com>
2022-01-11 21:23:10 -0500

    last modify

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 define('ROOT', dirname(__FILE__));
 require_once "class/connection/connection.php";
-require_once "class/databaseFunctions.php";
+require_once "class/databaseQuery.php";
 $function=new DatabaseQuery();
 $function->executeQuery("INSERT INTO `users` (`user`, `password`) VALUES ('test', 'test')");
 $data=$function->getData("SELECT * FROM `users`");
